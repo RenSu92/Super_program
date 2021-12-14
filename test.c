5cec8a2101f2d3156d07a78ec2e2d147403da54a
Renato Sudac <renato.su92@gmail.com>
2021-12-14 09:08:21 +0100

    Inicijalizacija projekta Dodane prve datoteke

---
+++ test.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(void){
+	int a,b;
+	printf("Upisi dva cijela broja kako bi utvrdio koji je veci:");
+	scanf("%d %d",&a,&b);
+	
+	if(a>b) printf("Broj %d je veci od %d.\n",a,b);
+	else if(a==b) printf("Brojevi su identicni.\n");
+	else printf("Broj %d je manji od broja %d\n",a,b);
+	
+	return 0;
+}
+
